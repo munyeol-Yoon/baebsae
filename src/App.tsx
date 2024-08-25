cf17b22248b86ec2990bac177b59bee74c7d48ed
muyeon <gmakin36@gmail.com>
2024-08-25 17:28:44 +0900

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <main className="flex justify-center items-center border border-black h-screen">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div>asd</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
