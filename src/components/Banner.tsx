380dc675d9715c8035fc8ccf11d424ee97896ce0
muyeon <gmakin36@gmail.com>
2024-08-28 19:45:10 +0900

    add: 배너 컴포넌트 완료

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,9 +2,25 @@ import BannerSVG from "./svg/BannerSVG";
 
 function Banner() {
   return (
-    <section className="w-full">
-      <BannerSVG width={"100%"} />
-    </section>
+    <>
+      <section className="w-full">
+        <BannerSVG width={"100%"} />
+      </section>
+      <div className="flex justify-start items-center flex-col gap-4 absolute text-white mt-20 sm:mt-28 md:mt-30 lg:mt-40 xl:mt-40">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl">
+          Invitation
+        </h1>
+        <span className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          최중수 | 유지영
+        </span>
+        <span className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+          D-33
+        </span>
+      </div>
+      <h1 className="text-sm sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl m-4">
+        Saturday, October 26, 2024 2:00PM
+      </h1>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ export default {
           "'AppleGothic'",
         ],
       },
+      fontSize: {
+        "fluid-h1": "clamp(1.5rem, 2vw + 1rem, 3rem)", // Adjust these values as needed
+        "fluid-base": "clamp(1rem, 1.5vw + 0.5rem, 2rem)",
+      },
     },
   },
   plugins: [],
